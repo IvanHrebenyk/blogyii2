610d59a1611ae4051a2f05d054c1f9319b76a10f
Ivan Hrebenyk <34397651+IvanHrebenyk@users.noreply.github.com>
2017-12-14 21:52:33 +0200

    Add files via upload

---
+++ migrations/m171213_221227_add_date_to_comment.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m171213_221227_add_date_to_comment
+ */
+class m171213_221227_add_date_to_comment extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('comment','date', $this->date());
+    }
+
+    public function down()
+    {
+        $this->dropColumn('comment','date');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
